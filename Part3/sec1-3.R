663a4b4cac6b0f3c8191bc59a7008b5e719882e7
youngmin0508 <sksmsehf@naver.com>
2019-05-29 15:19:36 +0900

    Part3, Section1-3

---
+++ Part3/sec1-3.R
@@ -0,0 +1,16 @@
+setwd("d:/Workspace/R_Data_Analysis/Part3/data")
+getwd()
+
+print(1+2)
+1+2
+print('a')
+"a"
+'string'
+print(pi)
+print(pi, digits=3)
+
+print(3,4)      #하나만 찍힌다
+print('a', 'b')     #에러가 발생한다
+cat(1,':','a','\n',2,':','b')
+3; 4; 5;
+1+2; 3*4; pi*5*5
